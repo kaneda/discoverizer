4dd70fe49489bfc13a8d07007cc974ef7df32b43
kaneda <josh.k4ned4@gmail.com>
2014-10-04 14:10:28 -0400

    [dev-more-links] Add show more option - Show more option for total and live views - Changed out tabs for two-spaces - Style updates

--- extension.js
+++ extension.js
@@ -1,40 +1,50 @@
 appAPI.ready(function($) {
-	if (!appAPI.matchPages("stumbleupon.com") && !appAPI.matchPages("www.stumbleupon.com")) return;
-	
-	/*
-	* DOM additions
-	*/
-	// Include the CSS
-	appAPI.resources.includeCSS('css/summarizer.css', {
-		key:'intro',
-		color:'#365F91',
-		fontFamily:'Cambria',
-		fontSize:'14pt',
-		fontWeight:'bold'
-	});
-	
-	// Include the summarizer template
-	$(appAPI.resources.get('html/summary_template.html'))
-		.appendTo('#header-top');
-		
-	// Load the stumble discoveries JS I wrote
-	appAPI.resources.includeJS('js/stumble_upon.js');
-	
-	/*
-	* Logic
-	*/
-	$("#summary_button").on('click', function(e) {
-		e.preventDefault();
-		$("#discovery_summary").slideToggle(250);
-	});
-	
-	$("#discovery_submit").bind('click', function(e) {
-		e.preventDefault();
-		if($("#su_username").val() && $("#su_username").val().length > 0) {
-			stumblerDiscoveries($("#su_username").val());
-		} else {
-			alert("You must enter a username for this to work");
-		}
-	});
+  if (!appAPI.matchPages("stumbleupon.com") && !appAPI.matchPages("www.stumbleupon.com")) return;
+  
+  /*
+  * DOM additions
+  */
+  // Include the CSS
+  appAPI.resources.includeCSS('css/summarizer.css', {
+    key:'intro',
+    color:'#365F91',
+    fontFamily:'Cambria',
+    fontSize:'14pt',
+    fontWeight:'bold'
+  });
+  
+  // Include the summarizer template
+  $(appAPI.resources.get('html/summary_template.html'))
+    .appendTo('#header-top');
+    
+  // Load the stumble discoveries JS I wrote
+  appAPI.resources.includeJS('js/stumble_upon.js');
+  
+  /*
+  * Logic
+  */
+  $("#summary_button").on('click', function(e) {
+    e.preventDefault();
+    $("#discovery_summary").slideToggle(250);
+  });
+
+  $("#show_more_total_button").on('click', function(e) {
+    e.preventDefault();
+    $("#show_more_total").slideToggle(250);
+  });
+  
+  $("#show_more_live_button").on('click', function(e) {
+    e.preventDefault();
+    $("#show_more_live").slideToggle(250);
+  });
+  
+  $("#discovery_submit").bind('click', function(e) {
+    e.preventDefault();
+    if($("#su_username").val() && $("#su_username").val().length > 0) {
+      stumblerDiscoveries($("#su_username").val());
+    } else {
+      alert("You must enter a username for this to work");
+    }
+  });
 });
 
